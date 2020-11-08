edfd397c8b3b151acf636ddc3da36b482eef34ab
Nadym <nadym.mallek@gmail.com>
2020-11-08 22:05:01 +0100

    enemies are moving have to change strategy of moving

--- enemy.py
+++ enemy.py
@@ -1,39 +1,54 @@
 import numpy as np
 
-class enemy:
+class Enemy:
     def __init__(self, name, position):
         self.name = name #a name to differenciate ennemies
-        self. position = position #position in the map
+        self.position = position #position in the map
 
     def update(self, current_state, neightboors):#Fonction qui décide comment bouger
         (x1, y1) = current_state[self.name] 
         (x2, y2) = current_state["Player"] 
-        n1 = np.sqrt(x1*x1+y1*y1) 
+        n1 = np.sqrt(x1*x1+y1*y1)
         n2 = np.sqrt(x2*x2+y2*y2)
-        angle = np.acos((x1*x2+y1*y2)/(n1*n2)) * 180 / np.pi
+        angle = np.arccos((x1*x2+y1*y2)/(n1*n2)) * 180 / np.pi
         dist = np.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1))
+        
+        #print("Je suis ", self.name, "\n Angle :", angle, "\nDistance :", dist)
         P = np.zeros(4)#Probabilities to go East, South, West, North in that order
+        print("Je suis", self.name, self.position,"\n Voici mes voisins", neightboors)
         for i in range(len(neightboors)):
-            if neightboors[i] == 0:#obstacle
+            if neightboors[i] == (-1, -1):#obstacle or out of bounds
                 P[i] = 0
             else:
                 P[i] = np.exp(0.33*self.W(angle)*self.T(dist))
+
+        print("Je suis ", self.name, "\n P", P)       
         som = np.sum(P)
         A = P/som
-
-        choice = np.random.choice([0, 1, 2, 3], p=A)[0]
+        print("Je suis ", self.name, "\n A", A)  
+        choice = np.random.choice([0, 1, 2, 3], p=A)
+        #print(choice)
         if choice == 0:#east
+            print("Je vais à l'est")
+            self.position = (x1, y1+1)
             current_state[self.name] = (x1, y1+1)
         elif choice == 1:#south
+            print("Je vais au sud")
+            self.position = (x1+1, y1)
             current_state[self.name] = (x1+1, y1)
         elif choice == 2:#west
+            print("Je vais à l'ouest")
+            self.position = (x1, y1-1)
             current_state[self.name] = (x1, y1-1)
         elif choice == 3:#north
+            print("Je vais au nord")
+            self.position = (x1-1, y1)
             current_state[self.name] = (x1-1, y1)
+        return current_state
     
     def W(self, angle):
         #function from article
-        return (180 - np.abs(angle)/180)
+        return (180 - np.abs(angle))/180
     
     def T(self, dist):
         #function from article
@@ -44,3 +59,5 @@ class enemy:
         else:
             return 1
         
+    def toString(self):
+        return "Je suis "+self.name+" et je me situe en (",self.position[0], self.position[1],")"
--- map.py
+++ map.py
@@ -1,5 +1,8 @@
 import sys
 import pygame
+import enemy
+import time
+import copy
 #import random
 #Les différentes cases
 O = 0 #Obstacle
@@ -20,21 +23,51 @@ LAND = pygame.image.load("images/grass.png")
 PLAYER = pygame.image.load("images/player.png")
 
 ENNEMY = pygame.image.load("images/vilain.png")
-TileColor = { 
-0 : OBSTACLE,
-F : FOOD, 
+
+TileColor = {
+O : OBSTACLE,
+F : FOOD,
 L : LAND,
 P : PLAYER,
-E : ENNEMY }
+E : ENNEMY
+}
 
-# MAP SIZES
+#Map Sizes (Pour l'affichage)
 TILESIZE = 25
 MAPWIDTH = 25
 MAPHEIGHT = 25
 
-#Création de la map A REMPLACER PAR UNE GENERATION ALEATOIRE
+#Création de la map de l'article
 
-map_artcile = [[O, O, O, O, O, O, L, L, L, L, L, L, L, L, L, L, L, L, L, O, O, O, O, O, O],
+blank_map = [
+[O, O, O, O, O, O, L, L, L, L, L, L, L, L, L, L, L, L, L, O, O, O, O, O, O],
+[O, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, O],
+[O, L, L, O, O, L, L, L, O, O, L, O, O, O, L, O, O, L, L, L, O, O, L, L, O],
+[O, L, O, O, O, L, L, L, O, O, L, O, L, O, L, O, O, L, L, L, O, O, O, L, O],
+[O, L, O, O, O, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, O, O, O, L, O],
+[O, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, O],
+[L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L],
+[L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L],
+[L, L, O, O, L, L, L, L, O, O, O, O, L, O, O, O, O, L, L, L, L, O, O, L, L],
+[L, L, O, O, L, L, L, L, O, O, O, L, L, L, O, O, O, L, L, L, L, O, O, L, L],
+[L, L, L, L, L, L, L, L, O, O, L, L, L, L, L, O, O, L, L, L, L, L, L, L, L],
+[L, L, O, O, L, L, L, L, O, L, L, L, L, L, L, L, O, L, L, L, L, O, O, L, L],
+[L, L, O, L, L, L, L, L, L, L, L, L, O, L, L, L, L, L, L, L, L, L, O, L, L],
+[L, L, O, O, L, L, L, L, O, L, L, L, L, L, L, L, O, L, L, L, L, O, O, L, L],
+[L, L, L, L, L, L, L, L, O, O, L, L, L, L, L, O, O, L, L, L, L, L, L, L, L],
+[L, L, O, O, L, L, L, L, O, O, O, L, L, L, O, O, O, L, L, L, L, O, O, L, L],
+[L, L, O, O, L, L, L, L, O, O, O, O, L, O, O, O, O, L, L, L, L, O, O, L, L],
+[L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L],
+[L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L],
+[O, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, O],
+[O, L, O, O, O, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, O, O, O, L, O],
+[O, L, O, O, O, L, L, L, O, O, L, O, L, O, L, O, O, L, L, L, O, O, O, L, O],
+[O, L, L, O, O, L, L, L, O, O, L, O, O, O, L, O, O, L, L, L, O, O, L, L, O],
+[O, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, O],
+[O, O, O, O, O, O, L, L, L, L, L, L, L, L, L, L, L, L, L, O, O, O, O, O, O]
+]
+map_article = [
+[O, O, O, O, O, O, L, L, L, L, L, L, L, L, L, L, L, L, L, O, O, O, O, O, O],
 [O, L, L, L, L, L, L, L, L, L, L, L, E, L, L, L, L, L, L, L, L, L, L, L, O],
 [O, L, L, O, O, L, L, L, O, O, L, O, O, O, L, O, O, L, L, L, O, O, L, L, O],
 [O, L, O, O, O, L, L, L, O, O, L, O, L, O, L, O, O, L, L, L, O, O, O, L, O],
@@ -58,9 +91,36 @@ map_artcile = [[O, O, O, O, O, O, L, L, L, L, L, L, L, L, L, L, L, L, L, O, O, O
 [O, L, O, O, O, L, L, L, O, O, L, O, L, O, L, O, O, L, L, L, O, O, O, L, O],
 [O, L, L, O, O, L, L, L, O, O, L, O, O, O, L, O, O, L, L, L, O, O, L, L, O],
 [O, F, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, L, F, F, O],
-[O, O, O, O, O, O, L, L, L, L, L, L, L, L, L, L, L, L, L, O, O, O, O, O, O]]
+[O, O, O, O, O, O, L, L, L, L, L, L, L, L, L, L, L, L, L, O, O, O, O, O, O]
+]
 
-print(map_artcile[1][12])
+current_state = { 
+"Player" : (18, 12),
+"Enemy1" : (1,12),
+#"Enemy2" : (6, 6),
+#"Enemy3" : (6, 12),
+#"Enemy4" : (6, 18),
+"Food1" : (4, 9),
+"Food2" : (4, 19),
+"Food3" : (5, 20),
+"Food4" : (10, 14),
+"Food5" : (10, 17),
+"Food6" : (10, 22),
+"Food7" : (11, 0),
+"Food8" : (13, 0),
+"Food9" : (13, 14),
+"Food10": (14, 2),
+"Food11": (14, 4),
+"Food12": (14, 12),
+"Food13": (14, 17),
+"Food14": (20, 5),
+"Food15": (23, 1),
+"Food16": (23, 22),
+"Food17": (23, 23)
+}
+
+
+
 """
 RANDOM MAP
 
@@ -69,17 +129,59 @@ for i in range(MAPWIDTH):
     for j in range(MAPHEIGHT):
         map1[i].append(random.choice(elems))
 """
+def map_fusion(current_state, blank_map):
+    map_vide = copy.deepcopy(blank_map)
+    for elem in current_state:
+        if elem.startswith("F"):
+            map_vide[current_state[elem][0]][current_state[elem][1]] = F
+        elif elem.startswith("E"):
+            map_vide[current_state[elem][0]][current_state[elem][1]] = E
+        elif elem.startswith("P"):
+            map_vide[current_state[elem][0]][current_state[elem][1]] = P
+    return map_vide
 
-#Map Sizes (Pour l'affichage)
+def neightboors(x,y, blank_map):
+    ret = []
+    if y < 24 and blank_map[x][y+1] != O :
+        ret.append((x,y+1))#east
+    else:
+        ret.append((-1,-1))
+    if x < 24 and blank_map[x+1][y] != O :
+        ret.append((x,y+1))#south
+    else:
+        ret.append((-1, -1))
+    if y > 0 and blank_map[x][y-1] != O :
+        ret.append((x,y-1))#west
+    else:
+        ret.append((-1, -1))
+    if x > 0 and blank_map[x-1][y] != O :
+        ret.append((x-1,y))#north
+    else:
+        ret.append((-1, -1))
 
-#CREATE DISPLAY (affichage)
+    return ret
 
+def update_states(current_state, enemies, blank_map):
+    for enemy in enemies:
+        current_state = enemy.update(current_state, neightboors(enemy.position[0], enemy.position[1], blank_map))
+    return current_state
 pygame.init()
 DISPLAY = pygame.display.set_mode((MAPWIDTH*TILESIZE, MAPHEIGHT*TILESIZE))
 
-#User Interface (à ne pas garder normalement)
+#Creation des ennemis
+enemies = []
+for elem in current_state:
+    if elem.startswith("E"):
+        enemies.append(enemy.Enemy(elem, (current_state[elem][0], current_state[elem][1])))
 
-while True:
+#print([enemy.toString() for enemy in enemies])
+"""for enemy in enemies:
+    current_state = enemy.update(current_state, neightboors(enemy.position[0], enemy.position[1]))
+"""
+#print([enemy.toString() for enemy in enemies])
+
+#CREATE DISPLAY (affichage)
+def update_disp(current_map):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -87,8 +189,16 @@ while True:
     #Drawing map
     for row in range(MAPHEIGHT):
         for col in range(MAPWIDTH):
+             
             DISPLAY.blit(
-                TileColor[map_artcile[row][col]],
+                TileColor[current_map[row][col]],
                 (col*TILESIZE, row*TILESIZE, TILESIZE, TILESIZE))
 
+#User Interface (à ne pas garder normalement)
+
+while True:
+    
+    update_disp(map_fusion(current_state, blank_map))     
     pygame.display.update()
+    current_state = update_states(current_state, enemies, blank_map)
+    time.sleep(0.5)
